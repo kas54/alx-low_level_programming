cd009c5619af9f90c953e05e5d4074c683b06181
kassahun shimelis <kasushimelis54@gmail.com>
2022-03-14 23:00:02 +0300

    all project

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,19 +4,19 @@ int main(void)
 {
 char ch = 'a';
 int i = 0;
- 	while (i < 16)
-	{
-		if (i < 10)
-		{
-			putchar(i + '0');
-		}
-		else
-		{
-		    putchar(ch);
-		    ch++;
-		}
-		i++;
-	}
-	putchar('\n');
-    return (0);
+while (i < 16)
+{
+if (i < 10)
+{
+putchar(i + '0');
+}
+else
+{
+putchar(ch);
+ch++;
+}
+i++;
+}
+putchar('\n');
+return (0);
 }
