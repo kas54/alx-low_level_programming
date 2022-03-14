f963cd4f36e32818de0e81d0c86ed5bc3473d115
kassahun shimelis <kasushimelis54@gmail.com>
2022-03-14 22:49:36 +0300

    all project

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	for (int i = 0; i <= 9; i++)
-	{
-		printf("%d", i);
-	}
-	printf("\n");
+   for (int i = 0; i <= 9; i++)
+     {
+       printf("%d", i);
+     }
+       printf("\n");
     return (0);
 }
